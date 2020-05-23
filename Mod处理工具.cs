adf2d301bdcfb41fa4c9e828e32bf6fb8ed61eb9
ThreeIce <THreeIce@outlook.com>
2020-05-23 11:36:46 +0800

    完成用户输入界面和json文件读取

---
+++ Mod处理工具.cs
@@ -0,0 +1,132 @@
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MC基岩版物品配方转方块配方工具
+{
+    public class ModTool
+    {
+        /// <summary>
+        /// 源mod行为包路径
+        /// </summary>
+        public string SBPath;
+        /// <summary>
+        /// 源mod材质包路径
+        /// </summary>
+        public string SRPath;
+        /// <summary>
+        /// 生成Mod行为包路径
+        /// </summary>
+        public string TBPath;
+        /// <summary>
+        /// 生成Mod材质包路径
+        /// </summary>
+        public string TRPath;
+
+        /// <summary>
+        /// 源mod行为包中物品目录
+        /// </summary>
+        protected string SBItemPath;
+        /// <summary>
+        /// 源mod行为包中合成配方目录
+        /// </summary>
+        protected string SBRecipesPath;
+        /// <summary>
+        /// 源Mod材质包中贴图目录
+        /// </summary>
+        protected string SRTexturesPath;
+        /// <summary>
+        /// 源mod材质包中物品贴图目录
+        /// </summary>
+        protected string SRTItemsPath;
+
+        /// <summary>
+        /// 生成Mod行为包中方块目录
+        /// </summary>
+        protected string TBBlockPath;
+        /// <summary>
+        /// 生成Mod行为包中合成配方目录
+        /// </summary>
+        protected string TBRecipesPath;
+        /// <summary>
+        /// 生成Mod行为包中掉落表目录
+        /// </summary>
+        protected string TBLootPath;
+
+        /// <summary>
+        /// 生成Mod材质包中记录Blocks贴图信息的json
+        /// </summary>
+        protected string TRBlocksJsonPath;
+        /// <summary>
+        /// 生成的Mod材质包中方块贴图目录
+        /// </summary>
+        protected string TRTBlocksPath;
+
+
+
+        /// <summary>
+        /// 源mod行为包物品的Json集合
+        /// </summary>
+        protected List<JObject> SourceItemsJson;
+
+        protected List<JObject> SourceRecipesJson;
+
+        public ModTool(string sbPath, string srPath, string tbPath, string trPath){
+            SBPath = sbPath;
+            SRPath = srPath;
+            TBPath = tbPath;
+            TRPath = trPath;
+            
+            SBItemPath = SBPath + @"\items";
+            SBRecipesPath = SBPath + @"\recipes";
+
+            SRTexturesPath = SRPath + @"\textures";
+            SRTItemsPath = SRPath + @"\item";
+
+            TBBlockPath = TBPath + @"\blocks";
+            TBRecipesPath = TBPath + @"\recipes";
+            TBLootPath = TBPath + @"\loot_tables";
+            
+            TRBlocksJsonPath = TRPath + "blocks.json";
+            TRTBlocksPath = TRPath + @"\textures\blocks";
+        }
+        /// <summary>
+        /// 加载源mod内需要的文件
+        /// </summary>
+        /// <returns></returns>
+        public void ReadFile(){
+            //加载所有物品文件
+            string[] files = Directory.GetFiles(SBItemPath,"*.json",SearchOption.AllDirectories);
+            System.Console.WriteLine(files.Length);
+            SourceItemsJson = new List<JObject>(files.Length);
+            for (int i = 0; i < files.Length;i++){
+                try{
+                    SourceItemsJson.Add(JObject.Parse(File.ReadAllText(files[i])));
+                }catch(Exception e){
+                    Console.WriteLine("错误：" + e.ToString());
+                    Console.WriteLine("序数：" + i);
+                    Console.WriteLine("路径：" + files[i]);
+                }
+            }
+            Console.WriteLine("共找到{0}个物品文件",SourceItemsJson.Count);
+            //加载所有配方文件
+            files = Directory.GetFiles(SBRecipesPath,"*.json",SearchOption.AllDirectories);
+            SourceRecipesJson = new List<JObject>();
+            for (int i = 0; i < files.Length;i++){
+                try{
+                    SourceRecipesJson.Add(JObject.Parse(File.ReadAllText(files[i])));
+                }catch(Exception e){
+                    Console.WriteLine("错误：" + e.ToString());
+                    Console.WriteLine("序数：" + i);
+                    Console.WriteLine("路径：" + files[i]);
+                }
+            } 
+            Console.WriteLine("共找到{0}个配方文件",SourceRecipesJson.Count);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MC基岩版物品配方转方块配方工具
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入目标mod行为包路径");
+            var sourcebpath = /*Console.ReadLine();*/@"H:\server\mcpe\mods\behaviour_packs\MoreFoodBP v009";
+            Console.WriteLine("请输入目标mod材质包路径");
+            var sourcerpath = /*Console.ReadLine()*/@"H:\server\mcpe\mods\resource_packs\MoreFoodRP v009";
+            Console.WriteLine("请输入生成Mod行为包路径");
+            var generatebpath = /*Console.ReadLine()*/"";
+            Console.WriteLine("请输入生成Mod材质包路径");
+            var generaterpath  = /*Console.ReadLine()*/"";
+            var tool = new ModTool(sourcebpath,sourcerpath,generatebpath,generaterpath);
+            tool.ReadFile();
+        }
+    }
+}
